6f8fe32e375e78b4087ad253ffbb676f8a342481
Jianming Zhao <jianmingzhao520@gmail.com>
2017-06-06 08:16:00 -0400

    delete agents test

--- delete_agent.php
+++ delete_agent.php
@@ -3,7 +3,7 @@ include("config.php");
 
 //delete agent from table
 $agent_id = $_GET['agent_id'];
-$result = mysqli_query($mysqli, "DELETE FROM agent WHERE agent_id=$agent_id");
+$result = mysqli_query($mysqli, "DELETE FROM agent WHERE agent_id='$agent_id'");
 
 //redirecting back to agent page
 header("Location:agent.php");
