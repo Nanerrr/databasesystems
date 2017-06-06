f8eddddabff58857453978effe774ff48846926d
Jianming Zhao <jianmingzhao520@gmail.com>
2017-06-06 07:04:33 -0400

    page redirections

--- homepage.php
+++ homepage.php
@@ -1,8 +1,8 @@
 <ul>
   <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/homepage.php">Main Page</a></li>
-  <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/">Agent</a></li>
-  <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/">Client</a></li>
-  <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/">Insurance Company</a></li>
-  <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/">Insurance Policy</a></li>
-  <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/">Premium Invoice</a></li>
+  <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/agent.php">Agent</a></li>
+  <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/client.php">Client</a></li>
+  <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/insurance_company.php">Insurance Company</a></li>
+  <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/insurance_policy">Insurance Policy</a></li>
+  <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/premium_invoice">Premium Invoice</a></li>
 </ul>
---
+++ premium_invoice.php
@@ -0,0 +1,3 @@
+<ul>
+  <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/homepage.php">Main Page</a></li>
+</ul>
