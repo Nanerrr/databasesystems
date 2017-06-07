0923d9a070d0b2e6c0760d455ab8d459fb0bedb7
Jianming Zhao <jianmingzhao520@gmail.com>
2017-06-07 05:56:23 -0400

    added premium invoice

--- add_premium_invoice.php
+++ add_premium_invoice.php
@@ -5,14 +5,14 @@ include_once("config.php");
 
 if(isset($_POST['Submit'])) {
 	//list of parameters
-	$invoice_id = mysqli_real_escape_string($mysqli, $_POST['invoice_id']);
 	$insured_name = mysqli_real_escape_string($mysqli, $_POST['insured_name']);
-	$address = mysqli_real_escape_string($mysqli, $_POST['address']);
 	$date_due = mysqli_real_escape_string($mysqli, $_POST['date_due']);
 	$amount_due = mysqli_real_escape_string($mysqli, $_POST['amount_due']);
-		
+	$address = mysqli_real_escape_string($mysqli, $_POST['address']);
+	$invoice_id = mysqli_real_escape_string($mysqli, $_POST['invoice_id']);
+
 	//insert invoice to database	
-	$result = mysqli_query($mysqli, "INSERT INTO premium_invoice (invoice_id, insured_name, address, date_due, amount_due) VALUES('$invoice_id', '$insured_name', '$address', '$date_due', '$amount_due'");
+	$result = mysqli_query($mysqli, "INSERT INTO insurance_company (insured_name, date_due, amount_due, address, invoice_id) VALUES('$insured_name', '$date_due', '$amount_due', '$address', '$invoice_id')");
 		
 		
 	//display success insert message
