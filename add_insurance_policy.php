0090d96ff5b91bfb5aef0b8c4f7b38d61d6e35d5
Jianming Zhao <jianmingzhao520@gmail.com>
2017-06-07 07:07:55 -0400

    added insurance policy

--- add_insurance_policy.php
+++ add_insurance_policy.php
@@ -12,11 +12,11 @@ if(isset($_POST['Submit'])) {
 	$policy_coverage = mysqli_real_escape_string($mysqli, $_POST['policy_coverage']);
 	$premium = mysqli_real_escape_string($mysqli, $_POST['premium']);
 	$start_date = mysqli_real_escape_string($mysqli, $_POST['start_date']);
-	$end_date = mysqli_real_escape_string($mysqli, $_POST['end_date']);
+	$expiration_date = mysqli_real_escape_string($mysqli, $_POST['expiration_date']);
 	$company_name = mysqli_real_escape_string($mysqli, $_POST['company_name']);
 		
 	//insert policy to database	
-	$result = mysqli_query($mysqli, "INSERT INTO insurance_policy(policy_id, name, phone, policy_type, policy_coverage, premium, start_date, end_date, company_name) VALUES('$policy_id', '$name', '$phone', '$policy_type', '$policy_coverage', '$premium', '$start_date', '$end_date', '$company_name')");
+	$result = mysqli_query($mysqli, "INSERT INTO insurance_policy(policy_id, name, phone, policy_type, policy_coverage, premium, start_date, expiration_date, company_name) VALUES('$policy_id', '$name', '$phone', '$policy_type', '$policy_coverage', '$premium', '$start_date', '$expiration_date', '$company_name')");
 		
 	//display success insert message
 	echo "<font color='green'>Insurance policy added successfully.";
