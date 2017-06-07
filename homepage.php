e3143709f8188637f3fb61c8da1d4d22205b5904
Jianming Zhao <jianmingzhao520@gmail.com>
2017-06-07 06:23:33 -0400

    updated homepage

--- homepage.php
+++ homepage.php
@@ -1,8 +1,8 @@
 <a href="http://webtech.kettering.edu/~zhao8671/databasesystems/homepage.php">Main Page</a>
-<ul>
+<ol>
   <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/agent.php">Agent</a></li>
   <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/client.php">Client</a></li>
   <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/insurance_company.php">Insurance Company</a></li>
   <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/insurance_policy">Insurance Policy</a></li>
   <li><a href="http://webtech.kettering.edu/~zhao8671/databasesystems/premium_invoice">Premium Invoice</a></li>
-</ul>
+</ol>
